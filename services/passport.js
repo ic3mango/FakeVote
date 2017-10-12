f312db52416968ed493d401d303a46f32ae11c6f
Stefan <stefan@apples-MacBook-Pro.local>
2017-10-12 20:45:05 +0200

    debug32 added console logs to code

--- services/passport.js
+++ services/passport.js
@@ -25,6 +25,7 @@ passport.use(
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ googleId: profile.id });
+      console.log('reached google auth');
 
       if (existingUser) {
         return done(null, existingUser);
